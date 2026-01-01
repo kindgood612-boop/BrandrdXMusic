e3c913d88d87ccab29b0d146c87a071162cba258
kindgood612-boop <kindgood612@gmail.com>
2026-01-01 20:14:53 +0200

    Update ping.py

--- BrandrdXMusic/plugins/tools/ping.py
+++ BrandrdXMusic/plugins/tools/ping.py
@@ -11,18 +11,35 @@ from BrandrdXMusic.utils.inline import supp_markup
 from config import BANNED_USERS, PING_IMG_URL
 
 
-@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
+@app.on_message(filters.command(["بينج", "سرعة", "حياة", "ping", "alive"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
+    
+    # رسالة الانتظار الأولى
     response = await message.reply_photo(
         photo=PING_IMG_URL,
-        caption=_["ping_1"].format(app.mention),
+        caption="**جـاري قـيـاس سـرعـة الـبـوت...** 🤍",
     )
+    
+    # جلب الإحصائيات من السيرفر والمساعد
     pytgping = await Hotty.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
+    
+    # تعديل الرسالة بالمعلومات النهائية
     await response.edit_text(
-        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
+        f"""
+**سـرعـة بـوت مـيـوزك** 🧚
+
+**🧚 سـرعـة الاسـتـجـابـة :** {resp} مـلـي ثـانـيـة
+**🤍 سـرعـة الـمـسـاعـد :** {pytgping}
+**⚡ وقـت الـتـشـغـيـل :** {UP}
+**🥀 الـرامـات :** {RAM}
+**💞 الـمـعـالـج :** {CPU}
+**♥️ الـمـسـاحـة :** {DISK}
+
+**{app.mention}** 🤍
+""",
         reply_markup=supp_markup(_),
     )
