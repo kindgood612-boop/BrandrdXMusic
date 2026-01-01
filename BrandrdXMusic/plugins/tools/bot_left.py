b62247be8c43c63be991d6634dee600b241b36d7
kindgood612-boop <kindgood612@gmail.com>
2026-01-01 16:46:12 +0200

    Update bot_left.py

--- BrandrdXMusic/plugins/tools/bot_left.py
+++ BrandrdXMusic/plugins/tools/bot_left.py
@@ -14,12 +14,13 @@ from BrandrdXMusic import app
 from BrandrdXMusic.utils.database import get_assistant
 from BrandrdXMusic.utils.database import delete_served_chat
 
+# الروابط الجديدة للصور
 photo = [
-    "https://graph.org/file/872dc8af2a36bed43b9b6.jpg",
-    "https://graph.org/file/f4b34351a59061ba1c61b.jpg",
-    "https://graph.org/file/3fb3f4c8a1250c6a50af1.jpg",
-    "https://graph.org/file/eabab7e8a3e5df87a0b04.jpg",
-    "https://graph.org/file/427f4869a158126957747.jpg",
+    "https://files.catbox.moe/wqipfn.jpg",
+    "https://files.catbox.moe/4qhfqw.jpg",
+    "https://files.catbox.moe/b6533n.jpg",
+    "https://files.catbox.moe/b91yyd.jpg",
+    "https://files.catbox.moe/xi3mb1.jpg",
 ]
 
 
@@ -31,14 +32,20 @@ async def on_left_chat_member(_, message: Message):
         left_chat_member = message.left_chat_member
         if left_chat_member and left_chat_member.id == (await app.get_me()).id:
             remove_by = (
-                message.from_user.mention if message.from_user else "𝐔ɴᴋɴᴏᴡɴ 𝐔sᴇʀ"
+                message.from_user.mention if message.from_user else "مـسـتـخـدم مـجـهـول"
             )
             title = message.chat.title
             username = (
-                f"@{message.chat.username}" if message.chat.username else "𝐏ʀɪᴠᴀᴛᴇ 𝐂ʜᴀᴛ"
+                f"@{message.chat.username}" if message.chat.username else "مـجـمـوعـة خـاصـة"
             )
             chat_id = message.chat.id
-            left = f"✫ <b><u>#𝗟𝗘𝗙𝗧_𝗚𝗥𝗢𝗨𝗣</u></b> ✫\n\n𝐂ʜᴀᴛ 𝐓ɪᴛʟᴇ : {title}\n\n𝗖𝗛𝗔𝗧 𝗜𝗗 : {chat_id}\n\n𝗥𝗘𝗠𝗢𝗩𝗘𝗗 𝗕𝗬 : {remove_by}\n\n𝗕𝗢𝗧 : @{app.username}"
+            left = (
+                f"✫ **خـروج مـن مـجـمـوعـة** 🥀\n\n"
+                f"**اسـم الـمـجـمـوعـة :** {title}\n\n"
+                f"**آيـدي الـمـجـمـوعـة :** `{chat_id}`\n\n"
+                f"**تـم طـردي بـواسـطـة :** {remove_by}\n\n"
+                f"**الـبـوت :** @{app.username} 🤍"
+            )
             await app.send_photo(LOG_GROUP_ID, photo=random.choice(photo), caption=left)
             await delete_served_chat(chat_id)
             await userbot.leave_chat(chat_id)
