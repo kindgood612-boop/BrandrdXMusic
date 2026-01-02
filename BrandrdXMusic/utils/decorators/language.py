6051879d954c47dc00fc625fba0e68fd01bdc38c
kindgood612-boop <kindgood612@gmail.com>
2026-01-02 03:04:58 +0200

    Update language.py

--- BrandrdXMusic/utils/decorators/language.py
+++ BrandrdXMusic/utils/decorators/language.py
@@ -1,4 +1,6 @@
 from strings import get_string
+from BrandrdXMusic import app
+from config import SUPPORT_CHAT
 from BrandrdXMusic.misc import SUDOERS
 from BrandrdXMusic.utils.database import (get_lang, is_maintenance)
 
@@ -8,7 +10,7 @@ def language(mystic):
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
-                    text=f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ <a href={SUPPORT_CHAT}>sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ</a> ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                    text=f"{app.mention} تحت الصيانة حالياً 🥀، يمكنك زيارة <a href={SUPPORT_CHAT}>جروب الدعم</a> لمعرفة السبب.",
                     disable_web_page_preview=True,
                 )
         try:
@@ -31,7 +33,7 @@ def languageCB(mystic):
         if await is_maintenance() is False:
             if CallbackQuery.from_user.id not in SUDOERS:
                 return await CallbackQuery.answer(
-                    f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                    f"{app.mention} تحت الصيانة حالياً 🥀، زر جروب الدعم لمعرفة السبب.",
                     show_alert=True,
                 )
         try:
