f76814861e638997091f2af02b24a9653f53880e
kindgood612-boop <kindgood612@gmail.com>
2026-01-02 23:48:41 +0200

    Update events.py

--- BrandrdXMusic/utils/events.py
+++ BrandrdXMusic/utils/events.py
@@ -1,20 +1,23 @@
-import inspect
 import re
-from pathlib import Path
 from telethon import events
 from BrandrdXMusic import telethn
 
-
 def register(**args):
-    """Registers a new message."""
+    """Registers a new message with optional prefix."""
     pattern = args.get("pattern", None)
 
-    r_pattern = r"^[/!.]"
+    # تـعـريـف الـبـادئـات بـحـيـث تـكـون اخـتـيـاريـة
+    # الـرمـز ? يـعـنـي أن الـعـلامـة قـد تـكـون مـوجـودة أو لا
+    r_pattern = r"^[/!.]?"
 
+    # إضـافـة خـاصـيـة تـجـاهـل حـالـة الـأحـرف (Case Insensitive)
     if pattern is not None and not pattern.startswith("(?i)"):
         args["pattern"] = "(?i)" + pattern
 
-    args["pattern"] = pattern.replace("^/", r_pattern, 1)
+    if pattern:
+        # اسـتـبـدال "إجـبـار الـسـلاش" بـ "الـعـلامـة الاخـتـيـاريـة"
+        # إذا كـان الـبـاتـرن ^/play سـيـصـبـح ^[/!.]?play
+        args["pattern"] = pattern.replace("^/", r_pattern, 1)
 
     def decorator(func):
         telethn.add_event_handler(func, events.NewMessage(**args))
