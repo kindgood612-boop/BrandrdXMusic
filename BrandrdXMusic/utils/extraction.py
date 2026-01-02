f5eb05145bceaf52f2d504c964028f90d488ef6d
kindgood612-boop <kindgood612@gmail.com>
2026-01-02 23:43:08 +0200

    Update extraction.py

--- BrandrdXMusic/utils/extraction.py
+++ BrandrdXMusic/utils/extraction.py
@@ -1,17 +1,24 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
-
 from BrandrdXMusic import app
 
-
 async def extract_user(m: Message) -> User:
+    # الـتـحـقـق مـن الـرد عـلـى رِسـالـة أولاً
     if m.reply_to_message:
         return m.reply_to_message.from_user
-    msg_entities = m.entities[1] if m.text.startswith("/") else m.entities[0]
-    return await app.get_users(
-        msg_entities.user.id
-        if msg_entities.type == MessageEntityType.TEXT_MENTION
-        else int(m.command[1])
-        if m.command[1].isdecimal()
-        else m.command[1]
-    )
+
+    # الـتـأكـد مـن وجـود مـعـطـيـات بـعـد الأمـر لـتـجـنـب الأخـطـاء
+    if not m.command or len(m.command) < 2:
+        return None
+
+    # الـبـحـث عـن الـمـنـشـن الـنـصـي (الأسـمـاء الـزرقـاء بـدون يـوزر)
+    if m.entities:
+        for entity in m.entities:
+            if entity.type == MessageEntityType.TEXT_MENTION:
+                return entity.user
+
+    # مـحـاولـة جـلـب الـمـسـتـخـدم عـن طـريـق الـيـوزر أو الآيـدي
+    try:
+        return await app.get_users(m.command[1])
+    except Exception:
+        return None
