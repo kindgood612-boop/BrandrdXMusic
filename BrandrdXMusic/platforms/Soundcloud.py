ba9042e2ac155ed5749376639426f4d20efa6396
kindgood612-boop <kindgood612@gmail.com>
2026-01-03 14:09:19 +0200

    Update Soundcloud.py

--- BrandrdXMusic/platforms/Soundcloud.py
+++ BrandrdXMusic/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from BrandrdXMusic.core.formatters import seconds_to_min
+from BrandrdXMusic.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
