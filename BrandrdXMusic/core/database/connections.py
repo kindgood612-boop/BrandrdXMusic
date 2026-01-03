57f2768c2817f78b7e2797d9114c1239472699fe
kindgood612-boop <kindgood612@gmail.com>
2026-01-03 07:00:22 +0200

    Create connections.py

---
+++ BrandrdXMusic/core/database/connections.py
@@ -0,0 +1,28 @@
+from BrandrdXMusic.core.mongo import mongodb
+
+authdb = mongodb.adminauth
+authuserdb = mongodb.authuser
+autoenddb = mongodb.autoend
+assdb = mongodb.assistants
+blacklist_chatdb = mongodb.blacklistChat
+blockeddb = mongodb.blockedusers
+chatsdb = mongodb.chats
+channeldb = mongodb.cplaymode
+countdb = mongodb.upcount
+gbansdb = mongodb.gban
+langdb = mongodb.language
+onoffdb = mongodb.onoffper
+playmodedb = mongodb.playmode
+playtypedb = mongodb.playtypedb
+skipdb = mongodb.skipmode
+sudoersdb = mongodb.sudoers
+usersdb = mongodb.tgusersdb
+privatedb = mongodb.privatechats
+suggdb = mongodb.suggestion
+cleandb = mongodb.cleanmode
+queriesdb = mongodb.queries
+userdb = mongodb.userstats
+videodb = mongodb.vipvideocalls
+chatsdbc = mongodb.chatsc
+usersdbc = mongodb.tgusersdbc
+chattopdb = mongodb.chattop
