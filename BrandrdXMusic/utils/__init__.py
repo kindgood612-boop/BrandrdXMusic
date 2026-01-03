df5f903add95592d122e6cfac78b8e92dcd6548b
kindgood612-boop <kindgood612@gmail.com>
2026-01-03 14:03:58 +0200

    Update __init__.py

--- BrandrdXMusic/utils/__init__.py
+++ BrandrdXMusic/utils/__init__.py
@@ -1,8 +1,10 @@
 from .channelplay import *
-from .database import *
 from .decorators import *
 from .extraction import *
 from .formatters import *
 from .inline import *
 from .pastebin import *
 from .sys import *
+
+# database اتحذف من utils واتنقل لـ core
+from BrandrdXMusic.core.database import *
