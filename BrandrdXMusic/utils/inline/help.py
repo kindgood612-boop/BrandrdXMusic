b3f7134610fba22b942263e419d81e29eca9ae6a
kindgood612-boop <kindgood612@gmail.com>
2026-01-03 12:33:21 +0200

    Update help.py

--- BrandrdXMusic/utils/inline/help.py
+++ BrandrdXMusic/utils/inline/help.py
@@ -4,123 +4,88 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from BrandrdXMusic import app
 
-    
 
 def help_pannel(_, START: Union[bool, int] = None):
-    first = [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close")]
-    second = [
+    # أزرار أسفل القائمة
+    close_row = [
+        InlineKeyboardButton(
+            text=_["CLOSE_BUTTON"],
+            callback_data="close",
+        )
+    ]
+
+    nav_row = [
         InlineKeyboardButton(
             text=_["BACK_PAGE"],
-            callback_data=f"mbot_cb",
+            callback_data="help_prev",
         ),
         InlineKeyboardButton(
             text=_["BACK_BUTTON"],
-            callback_data=f"settingsback_helper",
-        ),InlineKeyboardButton(
+            callback_data="settings_back_helper",
+        ),
+        InlineKeyboardButton(
             text=_["NEXT_PAGE"],
-            callback_data=f"mbot_cb",
+            callback_data="help_next",
         ),
     ]
-    mark = second if START else first
-    upl = InlineKeyboardMarkup(
+
+    last_row = nav_row if START else close_row
+
+    markup = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton(
-                    text=_["H_B_1"],
-                    callback_data="help_callback hb1",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_2"],
-                    callback_data="help_callback hb2",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_3"],
-                    callback_data="help_callback hb3",
-                ),
+                InlineKeyboardButton(_["H_B_1"], callback_data="help_callback hb1"),
+                InlineKeyboardButton(_["H_B_2"], callback_data="help_callback hb2"),
+                InlineKeyboardButton(_["H_B_3"], callback_data="help_callback hb3"),
             ],
             [
-                InlineKeyboardButton(
-                    text=_["H_B_4"],
-                    callback_data="help_callback hb4",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_5"],
-                    callback_data="help_callback hb5",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_6"],
-                    callback_data="help_callback hb6",
-                ),
+                InlineKeyboardButton(_["H_B_4"], callback_data="help_callback hb4"),
+                InlineKeyboardButton(_["H_B_5"], callback_data="help_callback hb5"),
+                InlineKeyboardButton(_["H_B_6"], callback_data="help_callback hb6"),
             ],
             [
-                InlineKeyboardButton(
-                    text=_["H_B_7"],
-                    callback_data="help_callback hb7",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_8"],
-                    callback_data="help_callback hb8",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_9"],
-                    callback_data="help_callback hb9",
-                ),
+                InlineKeyboardButton(_["H_B_7"], callback_data="help_callback hb7"),
+                InlineKeyboardButton(_["H_B_8"], callback_data="help_callback hb8"),
+                InlineKeyboardButton(_["H_B_9"], callback_data="help_callback hb9"),
             ],
             [
-                InlineKeyboardButton(
-                    text=_["H_B_10"],
-                    callback_data="help_callback hb10",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_11"],
-                    callback_data="help_callback hb11",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_12"],
-                    callback_data="help_callback hb12",
-                ),
+                InlineKeyboardButton(_["H_B_10"], callback_data="help_callback hb10"),
+                InlineKeyboardButton(_["H_B_11"], callback_data="help_callback hb11"),
+                InlineKeyboardButton(_["H_B_12"], callback_data="help_callback hb12"),
             ],
             [
-                InlineKeyboardButton(
-                    text=_["H_B_13"],
-                    callback_data="help_callback hb13",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_14"],
-                    callback_data="help_callback hb14",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_15"],
-                    callback_data="help_callback hb15",
-                ),
+                InlineKeyboardButton(_["H_B_13"], callback_data="help_callback hb13"),
+                InlineKeyboardButton(_["H_B_14"], callback_data="help_callback hb14"),
+                InlineKeyboardButton(_["H_B_15"], callback_data="help_callback hb15"),
             ],
-            mark,
+            last_row,
         ]
     )
-    return upl
+
+    return markup
 
 
 def help_back_markup(_):
-    upl = InlineKeyboardMarkup(
+    return InlineKeyboardMarkup(
         [
             [
                 InlineKeyboardButton(
                     text=_["BACK_BUTTON"],
-                    callback_data=f"settings_back_helper",
-                ),
+                    callback_data="settings_back_helper",
+                )
             ]
         ]
     )
-    return upl
 
 
 def private_help_panel(_):
-    buttons = [
+    return InlineKeyboardMarkup(
         [
-            InlineKeyboardButton(
-                text=_["S_B_4"],
-                url=f"https://t.me/{app.username}?start=help",
-            ),
-        ],
-    ]
-    return buttons
+            [
+                InlineKeyboardButton(
+                    text=_["S_B_4"],
+                    url=f"https://t.me/{app.username}?start=help",
+                )
+            ]
+        ]
+    )
