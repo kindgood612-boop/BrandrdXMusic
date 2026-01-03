903954989b97314732edc5f6234e3ca04e796127
kindgood612-boop <kindgood612@gmail.com>
2026-01-03 15:09:59 +0200

    Create auth.py

---
+++ BrandrdXMusic/core/database/auth.py
@@ -0,0 +1,75 @@
+from typing import Dict, List, Union
+from .collections import authdb, authuserdb, nonadmin
+
+async def check_nonadmin_chat(chat_id: int) -> bool:
+    user = await authdb.find_one({"chat_id": chat_id})
+    if not user:
+        return False
+    return True
+
+async def is_nonadmin_chat(chat_id: int) -> bool:
+    mode = nonadmin.get(chat_id)
+    if not mode:
+        user = await authdb.find_one({"chat_id": chat_id})
+        if not user:
+            nonadmin[chat_id] = False
+            return False
+        nonadmin[chat_id] = True
+        return True
+    return mode
+
+async def add_nonadmin_chat(chat_id: int):
+    nonadmin[chat_id] = True
+    is_admin = await check_nonadmin_chat(chat_id)
+    if is_admin:
+        return
+    return await authdb.insert_one({"chat_id": chat_id})
+
+async def remove_nonadmin_chat(chat_id: int):
+    nonadmin[chat_id] = False
+    is_admin = await check_nonadmin_chat(chat_id)
+    if not is_admin:
+        return
+    return await authdb.delete_one({"chat_id": chat_id})
+
+async def _get_authusers(chat_id: int) -> Dict[str, int]:
+    _notes = await authuserdb.find_one({"chat_id": chat_id})
+    if not _notes:
+        return {}
+    return _notes["notes"]
+
+async def get_authuser_names(chat_id: int) -> List[str]:
+    _notes = []
+    for note in await _get_authusers(chat_id):
+        _notes.append(note)
+    return _notes
+
+async def get_authuser(chat_id: int, name: str) -> Union[bool, dict]:
+    name = name
+    _notes = await _get_authusers(chat_id)
+    if name in _notes:
+        return _notes[name]
+    else:
+        return False
+
+async def save_authuser(chat_id: int, name: str, note: dict):
+    name = name
+    _notes = await _get_authusers(chat_id)
+    _notes[name] = note
+
+    await authuserdb.update_one(
+        {"chat_id": chat_id}, {"$set": {"notes": _notes}}, upsert=True
+    )
+
+async def delete_authuser(chat_id: int, name: str) -> bool:
+    notesd = await _get_authusers(chat_id)
+    name = name
+    if name in notesd:
+        del notesd[name]
+        await authuserdb.update_one(
+            {"chat_id": chat_id},
+            {"$set": {"notes": notesd}},
+            upsert=True,
+        )
+        return True
+    return False
