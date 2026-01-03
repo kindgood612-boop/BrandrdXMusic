c265358d16681de18257300fc316cc76c348c05b
kindgood612-boop <kindgood612@gmail.com>
2026-01-03 19:00:10 +0200

    Update queries.py

--- BrandrdXMusic/core/database/queries.py
+++ BrandrdXMusic/core/database/queries.py
@@ -1,11 +1,6 @@
-# BrandrdXMusic/core/database/queries.py
-
-from BrandrdXMusic.misc import db
+from BrandrdXMusic.core.misc import db
 
 async def set_queries(count: int):
-    """
-    حفظ عدد الاستعلامات (Placeholder للتوافق مع السورس القديم)
-    """
     try:
         db["queries"] = count
     except Exception:
