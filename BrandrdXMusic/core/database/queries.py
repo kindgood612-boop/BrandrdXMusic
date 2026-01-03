eb05cee5de397bb4f2b08a338dfd5699f44991bb
kindgood612-boop <kindgood612@gmail.com>
2026-01-03 19:10:32 +0200

    Update queries.py

--- BrandrdXMusic/core/database/queries.py
+++ BrandrdXMusic/core/database/queries.py
@@ -1,7 +1,20 @@
-from BrandrdXMusic.core.misc import db
+from .collections import queriesdb
 
-async def set_queries(count: int):
-    try:
-        db["queries"] = count
-    except Exception:
-        pass
+async def get_queries() -> int:
+    chat_id = 98324
+    data = await queriesdb.find_one({"chat_id": chat_id})
+    if not data:
+        return 0
+    return data.get("mode", 0)
+
+async def set_queries(mode: int):
+    chat_id = 98324
+    data = await queriesdb.find_one({"chat_id": chat_id})
+    if data:
+        mode = data.get("mode", 0) + mode
+
+    await queriesdb.update_one(
+        {"chat_id": chat_id},
+        {"$set": {"mode": mode}},
+        upsert=True
+    )
