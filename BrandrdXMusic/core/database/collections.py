2ffba3718da8eeca8e7341a7e60b54ecf70bf659
kindgood612-boop <kindgood612@gmail.com>
2026-01-03 15:09:23 +0200

    Create collections.py

---
+++ BrandrdXMusic/core/database/collections.py
@@ -0,0 +1,52 @@
+# هذا الملف يحتوي على تعريفات قواعد البيانات والمتغيرات الموجودة في الذاكرة (Cache)
+from BrandrdXMusic.core.mongo import mongodb, pymongodb
+
+# Database Collections
+authdb = mongodb.adminauth
+authuserdb = mongodb.authuser
+autoenddb = mongodb.autoend
+assdb = mongodb.assistants
+blacklist_chatdb = mongodb.blacklistChat
+blockeddb = mongodb.blockedusers
+chatsdb = mongodb.chats
+chattopdb = mongodb.chattopdb
+channeldb = mongodb.cplaymode
+countdb = mongodb.upcount
+gbansdb = mongodb.gban
+langdb = mongodb.language
+onoffdb = mongodb.onoffper
+playmodedb = mongodb.playmode
+playtypedb = mongodb.playtypedb
+skipdb = mongodb.skipmode
+sudoersdb = mongodb.sudoers
+usersdb = mongodb.tgusersdb
+privatedb = mongodb.privatechats
+suggdb = mongodb.suggestion
+cleandb = mongodb.cleanmode
+queriesdb = mongodb.queries
+userdb = mongodb.userstats
+videodb = mongodb.vipvideocalls
+chatsdbc = mongodb.chatsc  # for clone
+usersdbc = mongodb.tgusersdbc  # for clone
+
+# Memory Cache variables
+active = []
+activevideo = []
+assistantdict = {}
+autoend = {}
+count = {}
+channelconnect = {}
+langm = {}
+loop = {}
+maintenance = []
+nonadmin = {}
+pause = {}
+playmode = {}
+playtype = {}
+skipmode = {}
+privatechats = {}
+cleanmode = []
+suggestion = {}
+mute = {}
+audio = {}
+video = {}
